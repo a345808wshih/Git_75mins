a3eb65a4bef151d6946e0117694829b4297192cc
WSHIH <a34588+morvan@gmail.com>
2018-10-31 13:26:51 -0700

    Added main.cpp in dev

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char** argv) {
 	cout << "This is the master branch: stash test" << endl;
 	
 	cout << "modified by user2" << endl;
-	cout << "rebase test: in master" << endl;
+	cout << "rebase test: in master, this is bug-fixed" << endl;
+	cout << "rebaced test: in dev, this is the new-feature" << endl;
 
 	return 0;
 }
