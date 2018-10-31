7463b36a4ef2433e8a8bd416ccf829fe5e512d18
WSHIH <a34588+morvan@gmail.com>
2018-10-30 22:26:05 -0700

    modified main.cpp to test stash

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv) {
 	cout << "Hello WSHIH again" << endl;
 	cout << "This is the dev branch, merged dev into master" << endl;
 	cout << "This is the fix branch" << endl;
+	cout << "This is the master branch: stash test" << endl;
 
 	return 0;
 }
