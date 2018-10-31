0acfa744491c9a17fac18508bf502f6b9e332e3c
WSHIH <a34588+morvan@gmail.com>
2018-10-30 19:42:53 -0700

    modified ReadMe, main.cpp, deleted main5

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main(int argc, char** argv) {
 	cout << "Hello WSHIH again" << endl;
 	cout << "This is the dev branch, merged dev into master" << endl;
 	cout << "This is the fix branch" << endl;
-	cout << "This is the fix2 branch" << endl;
 
 	return 0;
 }
