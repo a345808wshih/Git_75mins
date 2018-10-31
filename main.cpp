0ab569eb621a1ca88c0c18e2c0556e0922e4668b
WSHIH <a34588+morvan@gmail.com>
2018-10-30 19:12:06 -0700

    This is fix branch

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
 	cout << "Hello WSHIH" << endl;
 	cout << "Hello WSHIH again" << endl;
 	cout << "This is the dev branch, merged dev into master" << endl;
+	cout << "This is the fix branch" << endl;
 
 	return 0;
 }
