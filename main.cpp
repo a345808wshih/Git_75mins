2a64feb48adaf9afb687ba255fb4c3a2b453d437
WSHIH <a34588+morvan@gmail.com>
2018-10-31 16:16:46 -0700

    retracking main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	cout << "Hello World" << endl;
+	cout << "Hello WSHIH" << endl;
+	cout << "Hello WSHIH again" << endl;
+	cout << "This is the dev branch, merged dev into master" << endl;
+	cout << "This is the fix branch" << endl;
+	cout << "This is the fix2 branch" << endl;
+
+	return 0;
+}
