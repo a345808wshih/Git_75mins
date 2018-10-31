64a939797d7fea9ef75c2c1a0f3ed3806ebc7f0f
WSHIH <a34588+morvan@gmail.com>
2018-10-30 19:01:16 -0700

    master branch

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char** argv) {
 	cout << "Hello World" << endl;
 	cout << "Hello WSHIH" << endl;
 	cout << "Hello WSHIH again" << endl;
+	cout << "This is the master branch" << endl;
 
 	return 0;
 }
